5a35e36129875941709d0c67f686d34f503e9e52
Dumitru Labont <dumitrulabont@yahoo.com>
2020-12-13 21:36:15 +0200

    Add project files.

---
+++ Labont_Dumitru_Lab10/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Labont_Dumitru_Lab10.Data;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+
+
+namespace Labont_Dumitru_Lab10
+{
+    public partial class App : Application
+    {
+        static ShopingListDatabase database;
+        public static ShopingListDatabase Database
+        {
+            get
+            {
+                if(database == null)
+                {
+                    database = new ShopingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShopingList.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Labont_Dumitru_Lab10/Data/ShopingListDatabase.cs
@@ -0,0 +1,44 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Labont_Dumitru_Lab10.Models;
+
+namespace Labont_Dumitru_Lab10.Data
+{
+    public class ShopingListDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+        public ShopingListDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<ShopList>().Wait();
+        }
+        public Task<List<ShopList>> GetShopListAsync()
+        {
+            return _database.Table<ShopList>().ToListAsync();
+        }
+        public Task<ShopList> GetShopAsync(int id)
+        {
+            return _database.Table<ShopList>()
+                .Where(i => i.ID == id)
+                .FirstOrDefaultAsync();
+        }
+        public Task<int> SaveShopListAstnc(ShopList slist)
+        {
+            if(slist.ID != 0)
+            {
+                return _database.UpdateAsync(slist);
+            }
+            else
+            {
+                return _database.InsertAsync(slist);
+            }
+        }
+        public Task<int> DeleteShopListAsync(ShopList slist)
+        {
+            return _database.DeleteAsync(slist);
+        }
+    }
+}
